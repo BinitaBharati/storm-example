03a1b44e8692c1cd80597e5b13b4eb31503221c9
Binita Bharati <binita.bharati@gmail.com>
2014-09-11 18:21:42 +0530

    Edited project clj file

--- project.clj
+++ project.clj
@@ -10,7 +10,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.apache.storm/storm-core "0.9.2-incubating"]
-                 ;[activemq-all/activemq-all "5.5.1"] ;; Not present in maven central, install in local maven repo
                  [org.apache.activemq/activemq-all "5.5.1"]
                  [org.slf4j/slf4j-log4j12 "1.5.8"]
                  ]
